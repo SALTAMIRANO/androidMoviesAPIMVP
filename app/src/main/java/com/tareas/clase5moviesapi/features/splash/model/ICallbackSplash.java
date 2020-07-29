9baa63884ebba8caf62bb80ce3d51f4658a77b9f
SEBA <saltamirano@hitcode.net>
2020-07-28 21:02:44 -0400

    finalizado

---
+++ app/src/main/java/com/tareas/clase5moviesapi/features/splash/model/ICallbackSplash.java
@@ -0,0 +1,5 @@
+package com.tareas.clase5moviesapi.features.splash.model;
+
+public interface ICallbackSplash {
+    void success();
+}
---
+++ app/src/main/java/com/tareas/clase5moviesapi/features/splash/model/IModelSplash.java
@@ -0,0 +1,5 @@
+package com.tareas.clase5moviesapi.features.splash.model;
+
+public interface IModelSplash {
+    boolean wait(ICallbackSplash callback);
+}
